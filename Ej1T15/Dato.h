24c8addbf1a4308e245c3c07721851398936ac69
fanabalia <fanabalia@gmail.com>
2013-01-09 19:23:28 +0100

    Primer commit.

---
+++ Ej1T15/Dato.h
@@ -0,0 +1,13 @@
+//
+//  Dato.h
+//  Ej1T15
+//
+//  Created by macbook on 09/01/13.
+//  Copyright (c) 2013 macbook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dato : NSObject
+
+@end
---
+++ Ej1T15/Dato.m
@@ -0,0 +1,13 @@
+//
+//  Dato.m
+//  Ej1T15
+//
+//  Created by macbook on 09/01/13.
+//  Copyright (c) 2013 macbook. All rights reserved.
+//
+
+#import "Dato.h"
+
+@implementation Dato
+
+@end
